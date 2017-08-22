b97896036d839b06fe5f25d98d12d8f71c5bda49
Jan Prachař <jan.prachar@gmail.com>
2017-08-22 19:28:32 +0200

    Revert "Build apps/progs.h dynamically"
    This reverts commit be9d5484be644c0fa8df2ba1232624a0c84a893b.

--- apps/progs.pl
+++ apps/progs.pl
@@ -11,21 +11,12 @@
 
 use strict;
 use warnings;
-use lib '.';
-use configdata qw/@disablables %unified_info/;
+use configdata qw/@disablables/;
 
 my %commands = ();
 my $cmdre = qr/^\s*int\s+([a-z_][a-z0-9_]*)_main\(\s*int\s+argc\s*,/;
 
-my $apps_openssl = shift @ARGV;
-# because the program apps/openssl has object files as sources, and
-# they then have the corresponding C files as source, we need to chain
-# the lookups in %unified_info
-my @openssl_source =
-    map { @{$unified_info{sources}->{$_}} }
-    @{$unified_info{sources}->{$apps_openssl}};
-
-foreach my $filename (@openssl_source) {
+foreach my $filename (@ARGV) {
 	open F, $filename or die "Coudn't open $_: $!\n";
 	foreach (grep /$cmdre/, <F>) {
 		my @foo = /$cmdre/;
